1cb6847cd02b552daf4126e30decec84f3893718
nate <nrice@truecoders.io>
2020-02-05 01:04:50 -0600

    fixed nested class issue

---
+++ GenericsExercise/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GenericsExercise
+{
+    public class Employee : IEquatable<Employee>
+    {
+        public int Age;
+        public string Name;
+
+
+        public Employee(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public bool Equals(Employee other)
+        {
+            return (Age == other.Age && Name == other.Name);
+        }
+    }
+}
