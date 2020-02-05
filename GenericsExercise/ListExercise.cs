56d135fb3f0178bdc65a29215b41215c93c9d615
nate <nrice@truecoders.io>
2020-02-04 23:41:35 -0600

    answered the list class

--- GenericsExercise/ListExercise.cs
+++ GenericsExercise/ListExercise.cs
@@ -8,16 +8,21 @@ namespace GenericsExercise
         public static List<string> ProcessToKill(List<string> process)
         {
             // Create list of string with initial size to 3.
-          
+            var processToKill = new List<string>(3);
+
             // Show capacity ; here : 3.
+            Console.WriteLine(processToKill.Capacity);
 
             // Show number of items ; here : 0.
+            Console.WriteLine(processToKill.Count);
 
             /// TODO: 
-            /// Add items from process to processToKill list 
-            /// Process equals "Explorer.exe" don't be added, ignore it
-
-
+            /// Add items from process to processToKill list
+            foreach (var item in process)
+            {
+                processToKill.Add(item);
+            }
+            
             foreach (var p in processToKill)
             {
                 Console.WriteLine(p);
